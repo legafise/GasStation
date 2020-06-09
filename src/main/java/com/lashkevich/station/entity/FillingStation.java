ba011eddc02dd5dfce6836d6cf5fd69b2122606e
legafise <lashkevich1974@gmail.com>
2020-06-09 21:02:47 +0300

    Move strings in constants and redact filling station logic

--- src/main/java/com/lashkevich/station/entity/FillingStation.java
+++ src/main/java/com/lashkevich/station/entity/FillingStation.java
@@ -45,7 +45,7 @@ public class FillingStation {
 
     public void setFuel(BigDecimal fuel) throws FillingException {
         if (fuel.compareTo(Constant.MAX_STATION_FUEL) > 0 || fuel.compareTo(Constant.MIN_STATION_FUEL) < 0) {
-            throw new FillingException("Введено некорректное значение!");
+            throw new FillingException(Constant.ENTERED_INCORRECT_VALUE_ENTER_MESSAGE);
         }
 
         this.fuel = fuel;
@@ -55,22 +55,23 @@ public class FillingStation {
         try {
             this.isLocked = LOCK.tryLock(10, TimeUnit.SECONDS);
             BigDecimal fuelBeforeRefueling = car.getFuel();
-            LOGGER.info("Машина(" + car + ") подъехала к колонке (id = " + this.getId() + ")");
+            logCarWentFillingStation(car, this.getId());
 
             if (isLocked) {
                 BigDecimal finalAmount = car.getFuel().add(gasAmount);
                 if (finalAmount.compareTo(Constant.MAX_CAR_FUEL) > 0 ||
                         this.fuel.subtract(gasAmount).compareTo(Constant.MIN_STATION_FUEL) < 0 || gasAmount.compareTo(Constant.MIN_STATION_FUEL) <= 0) {
-                    throw new FillingException("В колонке не хватает бензина или введено некорректное значение бензина для заправки");
+                    throw new FillingException(Constant.NO_GAS_ENTER_MESSAGE + Constant.SPACE + Constant.OR + Constant.SPACE +
+                            Constant.ENTERED_INCORRECT_VALUE_ENTER_MESSAGE);
                 }
 
                 this.fuel = this.fuel.subtract(gasAmount);
                 car.setFuel(finalAmount);
-                Thread.sleep(3000);
-                LOGGER.info(car.getCarMake().getMakeName() + "(id = " + car.getId() + ")" + " заправлена на колонке id = " + this.getId() + "! Топливо = " + car.getFuel() +
-                        "%; Было заправлено " + car.getFuel().subtract(fuelBeforeRefueling) + "%");
+                Thread.sleep(2000);
+                logCarRefuelingInfo(car, fuelBeforeRefueling, this.id);
             } else {
-                LOGGER.info(car.getCarMake().getMakeName() + "(id = " + car.getId() + ") не дождалась своей очереди и уехала");
+                LOGGER.info(car.getCarMake().getMakeName() + Constant.LEFT_BRACKET + Constant.ID + Constant.SPACE + Constant.EQUALS +
+                        Constant.SPACE + car.getId() + Constant.RIGHT_BRACKET + Constant.SPACE + Constant.CAR_DID_NOT_WAIT_ENTER_MESSAGE);
             }
         } catch (InterruptedException e) {
             throw new FillingException(e);
@@ -83,23 +84,22 @@ public class FillingStation {
 
     public void fillFullTank(Car car) throws FillingException {
         try {
-            BigDecimal fuelBeforeRefueling = car.getFuel();
-            LOGGER.info("Машина(" + car + ") подъехала к колонке (id = " + this.getId() + ")");
             this.isLocked = LOCK.tryLock(10, TimeUnit.SECONDS);
+            BigDecimal fuelBeforeRefueling = car.getFuel();
+            logCarWentFillingStation(car, this.getId());
 
             if (isLocked) {
                 BigDecimal missingFuel = Constant.MAX_CAR_FUEL.subtract(car.getFuel());
                 if (this.fuel.subtract(missingFuel).compareTo(Constant.MIN_STATION_FUEL) < 0) {
-                    throw new FillingException("В колонке не хватает бензина");
+                    throw new FillingException(Constant.NO_GAS_ENTER_MESSAGE);
                 }
 
                 this.fuel = this.fuel.subtract(missingFuel);
                 car.setFuel(car.getFuel().add(missingFuel));
                 Thread.sleep(3000);
-                LOGGER.info(car.getCarMake().getMakeName() + "(id = " + car.getId() + ")" + " полностью заправлена на колонке id = " + this.getId() + "! Топливо = " + car.getFuel() +
-                        "%; Было заправлено " + car.getFuel().subtract(fuelBeforeRefueling) + "%");
+                logCarRefuelingInfo(car, fuelBeforeRefueling, this.id);
             } else {
-                LOGGER.info(car.getCarMake().getMakeName() + "(id = " + car.getId() + ") не дождалась своей очереди и уехала");
+                logCarLeaveInfo(car);
             }
         } catch (InterruptedException e) {
             throw new FillingException(e);
@@ -108,6 +108,26 @@ public class FillingStation {
                 LOCK.unlock();
             }
         }
+    }
+
+    private static void logCarRefuelingInfo(Car car, BigDecimal fuelBeforeRefueling, long id) {
+        LOGGER.info(car.getCarMake().getMakeName() + Constant.LEFT_BRACKET + Constant.ID + Constant.SPACE + Constant.EQUALS +
+                Constant.SPACE + car.getId() + Constant.RIGHT_BRACKET + Constant.SPACE + Constant.FILED_ON_FILLING_STATION_ENTER_MESSAGE +
+                Constant.SPACE + Constant.ID + Constant.SPACE + Constant.EQUALS + Constant.SPACE + id +
+                Constant.EXCLAMATION_POINT + Constant.SPACE + Constant.FUEL_GET_MESSAGE + Constant.SPACE + Constant.EQUALS +
+                Constant.SPACE + car.getFuel() + Constant.PERCENT + Constant.SEMICOLON + Constant.SPACE + Constant.HAS_BEEN_REFUELED_ENTER_MESSAGE +
+                Constant.SPACE + car.getFuel().subtract(fuelBeforeRefueling) + Constant.PERCENT);
+    }
+
+    private static void logCarLeaveInfo(Car car) {
+        LOGGER.info(car.getCarMake().getMakeName() + Constant.LEFT_BRACKET + Constant.ID + Constant.SPACE + Constant.EQUALS +
+                Constant.SPACE + car.getId() + Constant.RIGHT_BRACKET + Constant.SPACE + Constant.CAR_DID_NOT_WAIT_ENTER_MESSAGE);
+    }
+
+    private static void logCarWentFillingStation(Car car, long id) {
+        LOGGER.info(Constant.CAR_ENTER_MESSAGE + Constant.LEFT_BRACKET + car + Constant.RIGHT_BRACKET + Constant.SPACE +
+                Constant.WENT_TO_FILLING_STATION_ENTER_MESSAGE + Constant.SPACE + Constant.LEFT_BRACKET + Constant.ID +
+                Constant.EQUALS + id + Constant.RIGHT_BRACKET);
     }
 
     @Override
